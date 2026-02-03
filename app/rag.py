eb30fbf704aa437b3d2933b88ca680632638035a
rajanmishra18 <rajanmishra18@gmail.com>
2026-02-03 11:47:07 +0530

    Deployed app on docker

--- app/rag.py
+++ app/rag.py
@@ -5,7 +5,11 @@ from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_community.llms import Ollama
 
-VECTOR_DB_PATH = "vectorstore/faiss_index"
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+DATA_PATH = os.path.join(BASE_DIR, "data", "notes.txt")
+VECTOR_DB_PATH = os.path.join(BASE_DIR, "vectorstore", "faiss_index")
+
 
 # Load embeddings once
 embeddings = HuggingFaceEmbeddings(
@@ -20,7 +24,7 @@ def get_vectorstore():
             allow_dangerous_deserialization=True
         )
 
-    loader = TextLoader("data/notes.txt")
+    loader = TextLoader(DATA_PATH)
     documents = loader.load()
 
     splitter = RecursiveCharacterTextSplitter(
@@ -41,7 +45,11 @@ def answer_question(query: str) -> str:
     docs = VECTORSTORE.similarity_search(query, k=2)
     context = "\n".join([doc.page_content for doc in docs])
 
-    llm = Ollama(model="llama3")
+    llm = Ollama(
+    model="llama3",
+    base_url="http://host.docker.internal:11434"
+    )
+
 
     prompt = f"""
 Answer the question using ONLY the context below.
